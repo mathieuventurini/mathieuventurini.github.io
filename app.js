a2df5082af4f2dd901585845a2e3d0ebb7ed4c37
mathieuventurini <mathieu.venturini@gmail.com>
2021-03-17 20:05:21 +0100

    V1.4

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ let explicationMsg = document.querySelector('.explication-msg');
 let reloadPage = document.getElementById('reload');
 let allAnswers = document.querySelectorAll('.answer-check');
 let guessesChecked = document.querySelectorAll('.guesses');
+let exclamation = document.querySelectorAll('.exclamation');
+let exclamationOne = document.querySelector('.exclamation1');
+let exclamationTwo = document.querySelector('.exclamation2');
+let correctAnswerChecked = document.querySelector('.answer-check-correct')
 
 
 myForm.addEventListener("submit", (e) => {
@@ -24,7 +28,8 @@ for (let i = 0; i < allAnswers.length; i++) {
             } )
         })
     })
-}
+};
+
 
 // FUNCTION ON SUBMIT 
     function checked(){
@@ -39,33 +44,39 @@ for (let i = 0; i < allAnswers.length; i++) {
         for (let index = 0; index < wrongAnswer.length; index++) {
             if(wrongAnswer[index].classList.contains('guesses-checked')){
                 wrongAnswer[index].classList.add('wrong-answer')
+                wrongAnswer[index].classList.remove('guesses-checked')
             }
         };
 
         for (let e = 0; e < correctAnswer.length; e++) {
             if(correctAnswer[e].classList.contains('guesses-checked')){
-                console.log(correctAnswer);
+                correctAnswer[e].classList.add('correct-answer-checked')
+                
             }
             else if(valid){
-                correctAnswer[e].classList.add('wrong-answer');
+                correctAnswer[e].classList.add('correct-answer');
+                correctAnswer[e].style.color = 'red';
+                correctAnswer[e].style.fontWeight = '700';
+                
             }
+
         };
 
-        if (valid){
+        if(valid){
     
-            correctAnswer.forEach(correctAnswer => {
-                correctAnswer.classList.add('correct-answer');
-            });
             errorMsg.classList.remove('error-msg-active');
             explicationMsg.classList.add('explication-msg-active');
-            guessesChecked.forEach(guessesChecked => {
-                guessesChecked.classList.remove('guesses-checked')
-            })
+            submitBtn.style.display = 'none'
+            reloadPage.style.display = 'block'
+        
         }
+
         else {
             errorMsg.classList.add('error-msg-active');
     
-        };
+        }
+
+
     };
     
 reloadPage.addEventListener('click', (e)=> {
